1659e7be2f8931981fb3a3d6b8f13e43da231ad2
ellarieg1 <erieg@villanova.edu>
2025-04-22 18:43:29 -0400

    added logo and style to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/walserimage.png')}
           style={styles.reactLogo}
         />
       }>
@@ -33,10 +33,12 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 280,
+    width: 390,
     bottom: 0,
     left: 0,
+    marginTop: 2,
     position: 'absolute',
+    alignItems: 'left',
   },
 });
